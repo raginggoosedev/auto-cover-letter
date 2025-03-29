e384290c25537cd999de1d388709bf961be50e54
ExplorerDonutz <mwquick04@gmail.com>
2025-03-29 13:29:03 -0400

    Fix: Issues w/ jobs.py

--- backend/scraping/job.py
+++ backend/scraping/job.py
@@ -12,6 +12,7 @@ import random
 import pandas as pd
 import re
 
+
 class Job:
     company_name = ""
     description = ""
@@ -21,19 +22,21 @@ class Job:
     def __init__(self, url):
         url = self._get_job_api_url_from_job_posting_url(url)
         soup = BeautifulSoup(requests.get(url).text, "html.parser")
-        
+
         self.company_name = soup.find("a", {"class": "topcard__org-name-link topcard__flavor--black-link"}).text.strip()
 
         description_html = soup.find("div", {"class": "core-section-container__content break-words"})
-        
+
         elems = re.split('Description|Basic Qualifications|Preferred Qualifications', description_html.text)
 
         self.description = elems[1].strip()
-        self.basic_qualifications = elems[2].strip()
-        self.preferred_qualifications = elems[3].strip()
+        # self.basic_qualifications = elems[2].strip()
+        # self.preferred_qualifications = elems[3].strip()
 
     def _get_job_api_url_from_job_posting_url(self, url=""):
         # Check if job post URL is valid and convert to an API url
         pattern = re.compile("^https://www.linkedin.com/jobs/view/[0-9]+")
-        if pattern.match(url): return f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{url.split('/')[-1]}"
-        else: raise RuntimeException("Invalid job posting URL - must be a LinkedIn job post")
+        if pattern.match(url):
+            return f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{url.split('/')[-1]}"
+        else:
+            raise Exception("Invalid job posting URL - must be a LinkedIn job post")
